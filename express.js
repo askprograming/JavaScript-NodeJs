809e86bcff28824daadd8ace86e57200fdc113c4
Askprograming <gerenbit@gmail.com>
2019-05-02 08:26:29 -0700

    some change on express

--- express.js
+++ express.js
@@ -2,11 +2,11 @@
 //const http = require("http");
 //const fs = require("fs");
 const books =require('./lib/books')
-
+//const bodyParser = require("body-parser")
 const express = require("express");
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 4000);
 app.use(express.static(__dirname + '/public')); //set location for static files
 app.use(require("body-parser").urlencoded({extended: true}));//parse form submission
 
@@ -21,18 +21,20 @@ app.get('/', function(req,res){
 });
 
 
-// Handle GET 
-app.get('/delete', function(req,res){
-    let result = books.delete(req.query.title); // delete book object
-    res.render('delete', {title: req.query.title, result: result});
-});
+
 
 // Handle POST
-app.post('/get', function(req,res){
+app.post('/details', function(req,res){
     console.log(req.body)
-    var header = 'Searching for: ' + req.body.title + '<br>';
-    var found = books.get(req.body.title);
-    res.render("details", {title: req.body.title, result: found});
+    var header = 'Searching for:  ' + req.body.title + '<br>';
+    var found = books.get(req.body.searchtitle);
+    res.render("details", {title: req.body.searchtitle, result: found});
+});
+
+// Handle GET 
+app.get('/delete', function(req,res){
+    let result = books.delete(req.body.title); // delete book object
+    res.render('delete', {title: req.body.title, result: result});
 });
 
 // define 404 handler
@@ -43,5 +45,5 @@ app.use(function(req,res) {
 });
 
 app.listen(app.get('port'), function() {
-    console.log('Express started');    
+    console.log('express started');    
 });
